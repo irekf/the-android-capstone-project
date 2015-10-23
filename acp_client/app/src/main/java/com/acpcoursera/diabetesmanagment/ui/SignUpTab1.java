2ad355049cb3c1ea5be3fc174bdeb921c8a15f7a
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-10-24 00:38:55 +0500

    Add toggle buttons for the Teen/Follower choice

--- acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/SignUpTab1.java
+++ acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/SignUpTab1.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ToggleButton;
 
 import com.acpcoursera.diabetesmanagment.R;
 
@@ -15,6 +16,8 @@ public class SignUpTab1 extends Fragment {
     private static String TAG = SignUpTab1.class.getSimpleName();
 
     private FragmentTabHost mTabHost;
+    private ToggleButton mTeenButton;
+    private ToggleButton mFollowerButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +33,27 @@ public class SignUpTab1 extends Fragment {
                 if (isInputValid()) {
                     mTabHost.setCurrentTab(mTabHost.getCurrentTab() + 1);
                 }
+            }
+        });
+
+        mTeenButton = (ToggleButton) rootView.findViewById(R.id.sign_up_tab_1_teen_button);
+        mFollowerButton = (ToggleButton) rootView.findViewById(R.id.sign_up_tab_1_follower_button);
+
+        mTeenButton.setChecked(true);
+
+        mTeenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFollowerButton.setChecked(false);
+                mTeenButton.setChecked(true);
+            }
+        });
+
+        mFollowerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFollowerButton.setChecked(true);
+                mTeenButton.setChecked(false);
             }
         });
 
