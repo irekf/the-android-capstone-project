a57a97b970f41d691431cc3ae65831b30451b1b2
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-10-30 20:40:28 +0500

    Add a navigation drawer aka "Main Menu"

--- acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/MainActivity.java
+++ acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/MainActivity.java
@@ -4,11 +4,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.acpcoursera.diabetesmanagment.R;
 import com.acpcoursera.diabetesmanagment.service.NetOpsService;
@@ -16,20 +24,67 @@ import com.acpcoursera.diabetesmanagment.util.MiscUtils;
 
 import static com.acpcoursera.diabetesmanagment.util.MiscUtils.showToast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     private static String TAG = MainActivity.class.getSimpleName();
 
-    Button mLogOutButton;
+    TextView mLogOutButton;
 
     private NetOpsReceiver mReceiver;
+
+    private String[] mOptionTitles;
+    private DrawerLayout mDrawerLayout;
+    private ActionBar mActionBar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLogOutButton = (Button) findViewById(R.id.log_out_button);
+        mOptionTitles = getResources().getStringArray(R.array.option_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer_items);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mOptionTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectOption(position);
+            }
+        });
+
+        mActionBar = getSupportActionBar();
+
+        // drawer toggle
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                mActionBar.setTitle("Diabetes Management");
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                mActionBar.setTitle(R.string.label_main_menu);
+            }
+        };
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeButtonEnabled(true);
+
+        // logout button (not really a button for now)
+        mLogOutButton = (TextView) findViewById(R.id.log_out_button);
         mLogOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,6 +95,31 @@ public class MainActivity extends FragmentActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /* This callback allows to open/close the drawer by clicking on the toggle */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -74,6 +154,10 @@ public class MainActivity extends FragmentActivity {
             }
 
         }
+    }
+
+    private void selectOption(int position) {
+        // TODO change fragments depending on the item selected
     }
 
     private void logOut() {
