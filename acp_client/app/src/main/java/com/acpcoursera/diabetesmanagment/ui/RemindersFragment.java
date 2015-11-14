f261cfb051ec74e4c306057526b40ca73cbc2974
Irek Fakhrutdinov <irekfakh@gmail.com>
2015-11-14 13:16:57 +0500

    Add edit, delete capabilities to the reminders.

--- acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/RemindersFragment.java
+++ acp_client/app/src/main/java/com/acpcoursera/diabetesmanagment/ui/RemindersFragment.java
@@ -20,6 +20,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
 import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -34,6 +36,11 @@ import com.acpcoursera.diabetesmanagment.receiver.CheckInPublisher;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
+
+/*
+debugging in ADB:
+>adb shell dumpsys alarm > dump.txt
+ */
 
 public class RemindersFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -52,16 +59,14 @@ public class RemindersFragment extends Fragment implements LoaderManager.LoaderC
 
         getLoaderManager().initLoader(TEST_LOADER, null, this);
 
-        mReminders = (ListView) rootView.findViewById(R.id.reminders_list_view);
-
         mAdapter = new ReminderAdapter(getActivity(), null, 0);
-
+        mReminders = (ListView) rootView.findViewById(R.id.reminders_list_view);
         mReminders.setAdapter(mAdapter);
 
         setHasOptionsMenu(true);
 
-        initObligatoryReminders();
         mAlarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+        initObligatoryReminders();
 
         return rootView;
     }
@@ -163,9 +168,88 @@ public class RemindersFragment extends Fragment implements LoaderManager.LoaderC
 
                 Intent intent = new Intent(getActivity(), CheckInPublisher.class);
                 PendingIntent alarmIntent = PendingIntent
-                        .getBroadcast(getActivity(), reminderId, intent, 0);
+                        .getBroadcast(getActivity(), reminderId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
                 mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                         AlarmManager.INTERVAL_DAY, alarmIntent);
+                cursor.close();
+            }
+        }
+    }
+
+    private void editReminder(int id, int hourOfDay, int minute) {
+
+        ContentResolver cr = getActivity().getContentResolver();
+        ContentValues cvs = new ContentValues();
+        cvs.put(DmContract.Reminders.HOUR_OF_DAY, hourOfDay);
+        cvs.put(DmContract.Reminders.MINUTE, minute);
+        int rowsUpdated = cr.update(DmContract.Reminders.buildReminderUri(Integer.toString(id)),
+                cvs, null, null);
+        if (rowsUpdated != 0) {
+
+            Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                calendar.set(Calendar.MINUTE, minute);
+
+                Intent intent = new Intent(getActivity(), CheckInPublisher.class);
+                PendingIntent alarmIntent = PendingIntent
+                        .getBroadcast(getActivity(), id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                        AlarmManager.INTERVAL_DAY, alarmIntent);
+        }
+    }
+
+    private void deleteReminder(int id) {
+
+        ContentResolver cr = getActivity().getContentResolver();
+        int rowsDeleted = cr.delete(DmContract.Reminders.buildReminderUri(Integer.toString(id)),
+                null, null);
+        if (rowsDeleted != 0) {
+            Intent intent = new Intent(getActivity(), CheckInPublisher.class);
+            PendingIntent alarmIntent = PendingIntent
+                    .getBroadcast(getActivity(), id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            mAlarmManager.cancel(alarmIntent);
+        }
+    }
+
+    private void enableReminder(int id, boolean enabled) {
+
+        ContentResolver cr = getActivity().getContentResolver();
+        ContentValues cvs = new ContentValues();
+        cvs.put(DmContract.Reminders.IS_ENABLED, enabled);
+
+        Uri reminderUri = DmContract.Reminders.buildReminderUri(Integer.toString(id));
+        int rowsUpdated = cr.update(reminderUri, cvs, null, null);
+        if (rowsUpdated != 0) {
+            Cursor cursor = cr.query(reminderUri, null, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+
+                Intent intent = new Intent(getActivity(), CheckInPublisher.class);
+                PendingIntent alarmIntent = PendingIntent
+                        .getBroadcast(getActivity(), id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                if (enabled) {
+
+                    int hourOfDay = cursor
+                            .getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders.HOUR_OF_DAY));
+                    int minute = cursor
+                            .getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders.MINUTE));
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTimeInMillis(System.currentTimeMillis());
+                    calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                    calendar.set(Calendar.MINUTE, minute);
+
+                    mAlarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                            AlarmManager.INTERVAL_DAY, alarmIntent);
+                }
+                else {
+                    mAlarmManager.cancel(alarmIntent);
+                }
+
+            }
+
+            if (cursor != null) {
                 cursor.close();
             }
         }
@@ -186,13 +270,13 @@ public class RemindersFragment extends Fragment implements LoaderManager.LoaderC
         }
 
         @Override
-        public void bindView(View view, Context context, Cursor cursor) {
+        public void bindView(final View view, Context context, Cursor cursor) {
             TextView reminderId = (TextView) view.findViewById(R.id.reminder_id);
             ImageView reminderIcon = (ImageView) view.findViewById(R.id.reminder_icon);
             TextView reminderTime = (TextView) view.findViewById(R.id.reminder_time);
             Switch reminderEnabled = (Switch) view.findViewById(R.id.reminder_switch);
 
-            int id = cursor.getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders._ID));
+            final int id = cursor.getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders._ID));
             int hourOfDay = cursor.getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders.HOUR_OF_DAY));
             int minute = cursor.getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders.MINUTE));
             int isEnabled = cursor.getInt(cursor.getColumnIndexOrThrow(DmContract.Reminders.IS_ENABLED));
@@ -204,9 +288,54 @@ public class RemindersFragment extends Fragment implements LoaderManager.LoaderC
             SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm", Locale.US);
             reminderTime.setText(dateFormat.format(mCalendar.getTime()));
 
+            reminderEnabled.setOnCheckedChangeListener(null); // this prevents positive feedback
             reminderEnabled.setChecked(isEnabled != 0);
             reminderIcon.setImageResource(isEnabled != 0 ?
                     R.drawable.ic_alarm_on : R.drawable.ic_alarm_off);
+
+            // listeners
+            ImageView reminderEdit = (ImageView) view.findViewById(R.id.reminder_edit);
+            reminderEdit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Calendar calendar = Calendar.getInstance();
+                    TimePickerDialog dialog = new TimePickerDialog(getActivity(),
+                            new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                    editReminder(id, hourOfDay, minute);
+                                }
+                            },
+                            calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true);
+                    dialog.show();
+                }
+            });
+
+            ImageView reminderDelete = (ImageView) view.findViewById(R.id.reminder_delete);
+            reminderDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteReminder(id);
+                }
+            });
+
+            reminderEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    enableReminder(id, isChecked);
+                }
+            });
+
+        }
+    };
+
+    private class ReminderOnClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+
         }
     };
 
